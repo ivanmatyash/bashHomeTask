4c9082fdc6cb52f1810260baf93ee6daa3f35013
ivan Matyash <ivan.matyash97@gmail.com>
2016-11-28 02:56:05 +0300

    add solution of task

---
+++ get_opts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -lt 4 ]
+then
+echo "Few arguments!"
+exit 1
+fi
+
+while getopts "f:l:a" opt
+do
+case $opt in
+f) nameFile=$OPTARG;;
+l) line=$OPTARG;;
+a) check=1;;
+esac
+done
+
+if [ "$nameFile" -a "$line" ]
+then
+ if [ "$check" == 1 ]
+ then 
+  cat /dev/null > $nameFile
+ fi
+ echo "$(date +"%T")" $line >> $nameFile
+else
+ echo "You have not entered arguments -f and(or) -l "
+fi
